4893c629e7798ba59d704fc5ec14add9a7e4c9cd
Takayuki Sato <s.yukkiy@gmail.com>
2023-09-17 20:18:17 +0900

    feat: prepare a create app

--- WindowsExample/src/main.rs
+++ WindowsExample/src/main.rs
@@ -1,3 +1,57 @@
+#![windows_subsystem = "windows"]
+
+use windows::{
+    core::*,
+    ApplicationModel::{Core::*, Package},
+    Win32::UI::WindowsAndMessaging::*,
+    UI::Core::*,
+};
+
+#[implement(IFrameworkViewSource)]
+struct CoreApp();
+
+#[allow(non_snake_case)]
+impl IFrameworkViewSource_Impl for CoreApp {
+    fn CreateView(&self) -> Result<IFrameworkView> {
+        Ok(CoreAppView().into())
+    }
+}
+
+#[implement(IFrameworkView)]
+struct CoreAppView();
+
+#[allow(non_snake_case)]
+impl IFrameworkView_Impl for CoreAppView {
+    fn Initialize(&self, _: Option<&CoreApplicationView>) -> Result<()> {
+        Ok(())
+    }
+
+    fn SetWindow(&self, _: Option<&CoreWindow>) -> Result<()> {
+        Ok(())
+    }
+
+    fn Load(&self, _: &HSTRING) -> Result<()> {
+        Ok(())
+    }
+
+    fn Run(&self) -> Result<()> {
+        let window = CoreWindow::GetForCurrentThread()?;
+        window.Activate()?;
+
+        let dispatcher = window.Dispatchre()?;
+        dispatcher.ProcessEvents(CoreProcessEventsOption::ProcessUntilQuit)?;
+
+        Ok(())
+    }
+
+    fn Uninitialize(&self) -> Result<()> {
+        Ok(())
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    unsafe {
+        MessageBoxA(None, s!("Ansi"), s!("World"), MB_OK);
+        MessageBoxW(None, w!("Wide"), w!("World"), MB_OK);
+    }
 }
