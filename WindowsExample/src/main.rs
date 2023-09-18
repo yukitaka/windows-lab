14f56c92c7b9bd915964bb158c56687bcf32fb03
Takayuki Sato <s.yukkiy@gmail.com>
2023-09-18 23:45:30 +0900

    feat: modify a error message

--- WindowsExample/src/main.rs
+++ WindowsExample/src/main.rs
@@ -57,7 +57,7 @@ fn main() -> Result<()>{
         CoInitializeEx(None, COINIT_MULTITHREADED)?;
 
         if let Err(result) = Package::Current() {
-            MessageBoxW(None, w!("Error"), w!("World"), MB_OK);
+            MessageBoxW(None, w!("This sample must be registered (via register.cmd) and launched from Start."), w!("Error"), MB_OK);
             return Err(result);
         }
 
